4c0308494b432f43d43f720acf839906bc7fb6be
Alexsander Begeev <pishisudada11@gmail.com>
2026-04-22 00:44:44 +0500

    Initial commit

---
+++ src/main/java/com/example/komtekProject/KomtekProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.komtekProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomtekProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomtekProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/komtekProject/KomtekProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.komtekProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomtekProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
